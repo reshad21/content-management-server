628169accc1c5a92ab007cd207dd9e235fe27c96
reshad21 <miakala7@gmail.com>
2023-04-26 22:33:26 +0600

    delte api create

--- index.js
+++ index.js
@@ -42,6 +42,14 @@ async function run() {
             res.send(result);
         })
 
+        app.delete('/articls/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id);
+            const query = { _id: new ObjectId(id) }
+            const result = await articlesCollection.deleteOne(query);
+            res.send(result);
+        })
+
 
 
     }
