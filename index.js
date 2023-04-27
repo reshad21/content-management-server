46618aa33c7c1ecdabf500ff5c6c1e604ff1a78b
reshad21 <miakala7@gmail.com>
2023-04-27 21:44:23 +0600

    single content api create

--- index.js
+++ index.js
@@ -50,6 +50,13 @@ async function run() {
             res.send(result);
         })
 
+        app.get('/articls/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await articlesCollection.findOne(query)
+            res.send(result);
+        })
+
 
 
     }
